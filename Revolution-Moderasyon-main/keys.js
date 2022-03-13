5e12397b48bc44318fadce237d10745716f8edbc
erdem54x <78647402+erdem54x@users.noreply.github.com>
2022-03-13 22:38:42 +0300

    Update keys.js

--- Revolution-Moderasyon-main/keys.js
+++ Revolution-Moderasyon-main/keys.js
@@ -1,6 +1,6 @@
 const keys = {
-    "token": "",
-    "mongoose": "",
+    "token": "OTUyNjUwOTg4NzQ4MTY5Mzg3.Yi5HXw._VM_tPP1JvxHOsR0MxMTYmkC0dQ",
+    "mongoose": "mongodb+srv://Ardas:<545467>@cluster0.aizos.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     "hookLogs":{
     "discordUserLogID": "",
     "discordUserLogToken": "",
